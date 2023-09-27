dc5820f4b3338078102ced6b62c7b958a74c457e
Zulfiyya Qurbanova <tu7u71zbq@code.edu.az>
2023-09-27 13:54:45 +0400

    UNI

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿
+
+//task1
+
+//int a = 6;
+//int b = 9;
+//a = a + b;
+//b = a - b;
+//a = a - b;
+//Console.WriteLine(a+" "+b);
+
+
+//task2
+
+
+
+
+//int a = 77;
+//if (a==1)
+//{
+//    Console.WriteLine("hec biri deyil");
+//    return;
+//}
+//for (int i =2; i < a; i++)
+//{
+
+//    if(a%i==0 )
+//    {
+//        Console.WriteLine("murekkeb");
+//        return;
+//    }
+
+//}
+//Console.WriteLine("sade");
+
+
+
+//task3
+
+//Console.WriteLine("4 reqemli eded ver");
+
+// 
+
+//if (num % 10 == num/1000)
+//{
+//    Console.WriteLine( "palindrom ");
+//}
+//else
+//{
+//    Console.WriteLine("deyil");
+//}
+
+
+//int num = int.Parse(Console.ReadLine());
+//int temp = num;
+//int digit;
+//int reversed = 0;
+
+//while (temp>0)
+//{
+//    digit = temp % 10;
+//    reversed = (reversed * 10) + digit;
+//    temp /= 10;
+//}
+//if (num==reversed)
+//{
+//    Console.WriteLine("polindrom");
+//}
+//else
+//{
+//    Console.WriteLine("polindrom deyil");
+//}
